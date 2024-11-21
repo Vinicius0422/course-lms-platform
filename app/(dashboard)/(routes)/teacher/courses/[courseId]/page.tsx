245a90ed2762c850fefd3ffa4b9fca819bde5457
Vinicius <guedescosta123@hotmail.com>
2024-11-20 23:11:57 -0300

    feat: create attachment form component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,5 @@
 import { auth } from "@clerk/nextjs/server";
-import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
+import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
@@ -10,6 +10,7 @@ import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
+import { AttachmentForm } from "./_components/attachment-form";
 
 const CourseIdPage = async ({ params } : {
     params: {
@@ -26,6 +27,13 @@ const CourseIdPage = async ({ params } : {
     const course = await db.course.findUnique({
         where: {
             id: params.courseId
+        },
+        include: {
+            attachments: {
+                orderBy: {
+                    createdAt: "desc"
+                }
+            }
         }
     })
 
@@ -113,6 +121,18 @@ const CourseIdPage = async ({ params } : {
                         <PriceForm 
                             initialData={course}
                             courseId={course.id}/>
+                    </div>
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBagde icon={File}/>
+                            <h2 className="text-xl">
+                                Resources & Attachments
+                            </h2>
+                        </div>
+                        <AttachmentForm 
+                            initialData={course}
+                            courseId={course.id}
+                            />
                     </div>
                 </div>
             </div>
