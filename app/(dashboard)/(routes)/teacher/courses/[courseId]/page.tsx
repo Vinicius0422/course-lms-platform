5cc18830592383c3112477b4ea4a75675d13b532
Vinicius <guedescosta123@hotmail.com>
2024-11-20 21:27:54 -0300

    feat: create price form component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,5 @@
 import { auth } from "@clerk/nextjs/server";
-import { LayoutDashboard } from "lucide-react";
+import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
@@ -9,6 +9,7 @@ import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
+import { PriceForm } from "./_components/price-form";
 
 const CourseIdPage = async ({ params } : {
     params: {
@@ -68,7 +69,7 @@ const CourseIdPage = async ({ params } : {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
                 <div>
                     <div className="flex items-center gap-x-2">
-                        <IconBagde size="sm" variant="default" icon={LayoutDashboard}/>
+                        <IconBagde variant="default" icon={LayoutDashboard}/>
                         <h2 className="text-xl">
                             Customize your course
                         </h2>
@@ -89,6 +90,30 @@ const CourseIdPage = async ({ params } : {
                             label: category.name,
                             value: category.id
                         }))}/>
+                </div>
+                <div className="space-y-6">
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBagde icon={ListChecks}/>
+                            <h2 className="text-xl">
+                                Course chapters
+                            </h2>
+                        </div>
+                        <div>
+                            TODO: Chapters
+                        </div>
+                    </div>
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBagde icon={CircleDollarSign}/>
+                            <h2 className="text-xl">
+                                Sell your course
+                            </h2>
+                        </div>
+                        <PriceForm 
+                            initialData={course}
+                            courseId={course.id}/>
+                    </div>
                 </div>
             </div>
         </div>
